94eff1d2fbc4d7ca259f1d2044d9c8cd916e5a07
segmentationfaulter <segmentationfaulter@gmail.com>
2015-11-11 09:01:00 +0500

    V8 typed arrays

--- typed_arrays.js
+++ typed_arrays.js
@@ -1,12 +1,13 @@
 'use strict';
 
 process.stdin.once('data', (chunk) => {
-  const buf = new ArrayBuffer(chunk.length);  // this is coming from V8
+  // create the raw buffer
+  const buf = new ArrayBuffer(chunk.length);
 
-  // create a new view for the raw buffer
+  // create a new view for the raw buffer so that we could perform operations on raw buffer above
   let view = new Uint8Array(buf);
 
-  // copy octets from chunk coming from Node to typed array from V8
+  // copy octets from chunk coming from Node side to buffer coming from V8
   for (let i = 0; i < chunk.length; i++) {
     view[i] = chunk[i];
   }
